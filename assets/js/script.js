542d3db338a2936c22f932daf9d785138bea8857
IainJackson90 <>
2023-09-25 11:37:11 +0000

    Add: function commitedUserOption

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,56 @@
+/**
+ * The main gamme function that is loaded when the html loads
+ */
+document.addEventListener("DOMContentLoaded", function userOptions(){
+
+   let buttons = document.getElementsByTagName("button");
+   
+   for(let button of buttons) {
+    button.addEventListener("click", function commitedUserOption(){
+        
+       let user = this.getAttribute("data-type");
+       getBotOption();
+       console.log(user);
+       console.log(getBotOption(commitedUserOption));
+    //    return user;
+
+    })
+   }
+
+})
+
+/**
+ * This is the bot player it gets a random number and asings it a value of 
+ * rock, paper, scissors, lizard, spock.  
+ */
 function getBotOption() {
 
     let bot = Math.floor(Math.random() * 5) + 1;
     let comittedBotOption;
-   
-    if (bot === 1){
+
+    if (bot === 1) {
         comittedBotOption = "botRock";
-    } else if (bot ===2){
+    } else if (bot === 2) {
         comittedBotOption = "botPaper";
-    } else if (bot === 3){
+    } else if (bot === 3) {
         comittedBotOption = "botScissors";
-    } else if (bot === 4){
-        comittedBotOption = "botLizzard"
-    } else if (bot === 5){
+    } else if (bot === 4) {
+        comittedBotOption = "botLizzard";
+    } else if (bot === 5) {
         comittedBotOption = "botSpock";
     }
-      else {
+    else {
         alert("Something went wrong");
-    }  
+    }
 
     return comittedBotOption;
 }
 
+
+
+
+
+
+
+
+ 
