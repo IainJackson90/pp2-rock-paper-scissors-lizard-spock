7657a74e7eedaaf44f656acc3150c1d1454e2eb2
IainJackson90 <>
2023-09-25 13:46:17 +0000

    Add: Basick structure for function rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
 /**
  * The main gamme function that is loaded when the html loads
  */
-document.addEventListener("DOMContentLoaded", function userOptions(){
+document.addEventListener("DOMContentLoaded", function userOptions() {
 
-   let buttons = document.getElementsByTagName("button");
-   
-   for(let button of buttons) {
-    button.addEventListener("click", function commitedUserOption(){
-        
-       let user = this.getAttribute("data-type");
-       getBotOption();
-       console.log(user);
-       console.log(getBotOption(commitedUserOption));
-    //    return user;
+    let buttons = document.getElementsByTagName("button");
 
-    })
-   }
+    for (let button of buttons) {
+        button.addEventListener("click", function commitedUserOption() {
 
-})
+            let user = this.getAttribute("data-type");
+            getBotOption();
+            console.log(user);
+            console.log(getBotOption(commitedUserOption));
+            //    return user;
+
+        });
+    }
+
+});
 
 /**
  * This is the bot player it gets a random number and asings it a value of 
@@ -29,15 +29,15 @@ function getBotOption() {
     let comittedBotOption;
 
     if (bot === 1) {
-        comittedBotOption = "botRock";
+        comittedBotOption = "rock";
     } else if (bot === 2) {
-        comittedBotOption = "botPaper";
+        comittedBotOption = "paper";
     } else if (bot === 3) {
-        comittedBotOption = "botScissors";
+        comittedBotOption = "scissors";
     } else if (bot === 4) {
-        comittedBotOption = "botLizzard";
+        comittedBotOption = "lizzard";
     } else if (bot === 5) {
-        comittedBotOption = "botSpock";
+        comittedBotOption = "spock";
     }
     else {
         alert("Something went wrong");
@@ -47,10 +47,36 @@ function getBotOption() {
 }
 
 
+function rules(user, bot) {
+
+    let win = `You chose ${user} and the bot chose ${bot}, You win!`;
+    let lose = `You chose ${user} and the bot chose ${bot}, You Lose!`;
+
+    if (user === bot) {
+        return `You chose ${user} and the bot chose ${bot}, Draw!`;
+
+    } else if (user === "rock") {
+
+        if (bot === "paper") {
+         return lose;
+
+        } else if (bot === "spock") {
+            return lose;
+       
+        } else if (bot === "scissors"){
+            return win;
+
+        } else if (bot === "lizard"){
+            return win;
+        }
+
+    }
+
+
+}
 
 
 
 
 
 
- 
