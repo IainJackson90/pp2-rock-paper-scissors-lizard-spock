02c720e00d3c354e19428b0201008701f06f1256
IainJackson90 <>
2023-10-01 11:44:16 +0000

    Add: botStyling function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,16 @@ document.addEventListener("DOMContentLoaded", function userOptions() {
         button.addEventListener("click", function commitedUserOption() {
 
             let user = this.getAttribute("data-type");
-           const botOption =  getBotOption(); /* stores the value now*/
-           /*let  reulst = rules(user, botOption) */
-           let winningResult = rules(user, botOption);
+            const botOption = getBotOption(); /* stores the value now*/
+            /*let  reulst = rules(user, botOption) */
+            let winningResult = rules(user, botOption);
             console.log(user);
             console.log(botOption);
             console.log(winningResult);
             //    return user;
             document.getElementById("outcome").innerText = winningResult;
-           
+
+            botStyling(botOption);
             bestOutOfFive();
         });
     }
@@ -34,15 +35,18 @@ function getBotOption() {
     let bot = Math.floor(Math.random() * 5) + 1;
     let comittedBotOption;
     
-
     if (bot === 1) {
         comittedBotOption = "rock";
+
     } else if (bot === 2) {
         comittedBotOption = "paper";
+
     } else if (bot === 3) {
         comittedBotOption = "scissors";
+
     } else if (bot === 4) {
         comittedBotOption = "lizard";
+
     } else if (bot === 5) {
         comittedBotOption = "spock";
     }
@@ -78,12 +82,12 @@ function rules(user, bot) {
         } else if (bot === "spock") {
             botScore();
             return lose;
-       
-        } else if (bot === "scissors"){
+
+        } else if (bot === "scissors") {
             playerScore();
             return win;
 
-        } else if (bot === "lizard"){
+        } else if (bot === "lizard") {
             playerScore();
             return win;
         }
@@ -173,7 +177,7 @@ function rules(user, bot) {
 /**
  * This function incroments the score of the player
  */
-function playerScore(){
+function playerScore() {
 
     let oldPlayerScore = parseInt(document.getElementById("player-score-count").innerText);
     document.getElementById("player-score-count").innerText = ++oldPlayerScore;
@@ -199,34 +203,90 @@ function botScore() {
  * this function evaluates who got the best score out of five games
  * @returns 
  */
-function bestOutOfFive(){
+function bestOutOfFive() {
 
     let botScoreCount = parseInt(document.getElementById("bot-score-count").innerText);
     let playerScoreCount = parseInt(document.getElementById("player-score-count").innerText);
     let bestOutOff = botScoreCount + playerScoreCount;
-    
-    if(bestOutOff === 5){
-        if(botScoreCount > playerScoreCount){
-            alert("Bot won best out of 5 Games")
+
+    if (bestOutOff === 5) {
+        if (botScoreCount > playerScoreCount) {
+            alert("Bot won best out of 5 Games");
             document.getElementById("bot-score-count").innerText = 0;
             document.getElementById("player-score-count").innerText = 0;
 
-           return console.log("bestOutOfFive function bot won");
+            return console.log("bestOutOfFive function bot won");
 
-        } else if(botScoreCount < playerScoreCount) {
+        } else if (botScoreCount < playerScoreCount) {
             alert("Player won best out of 5 Games");
             document.getElementById("bot-score-count").innerText = 0;
             document.getElementById("player-score-count").innerText = 0;
 
-           return console.log("bestOutOfFive function player won");
+            return console.log("bestOutOfFive function player won");
 
-        }else {
+        } else {
             console.log("Something went wrong with bestOutOfFive() nested if statment ");
         }
     } else {
-        
+        /* do nothing  check if a case statment would work better*/
     }
 
+}
+
+/**
+ * This function will evaluate the choice of the comper and give the div of that choice 
+ * a boxShadow color of red
+ * @param {this parameter will be passed the argumrent of botOption} botStyle 
+ */
+function botStyling(botStyle) {
+
+   
+    if (botStyle === "rock") {
+        document.getElementById("bot-js-rock").style.boxShadow = "4px 4px 8px red";
+
+        document.getElementById("bot-js-paper").style.boxShadow = "none";
+        document.getElementById("bot-js-scissors").style.boxShadow = "none";
+        document.getElementById("bot-js-lizard").style.boxShadow = "none";
+        document.getElementById("bot-js-spock").style.boxShadow = "none";
+
+    } else if (botStyle === "paper") {
+        document.getElementById("bot-js-paper").style.boxShadow = "4px 4px 8px red";
+
+        document.getElementById("bot-js-rock").style.boxShadow = "none";
+        document.getElementById("bot-js-scissors").style.boxShadow = "none";
+        document.getElementById("bot-js-lizard").style.boxShadow = "none";
+        document.getElementById("bot-js-spock").style.boxShadow = "none";
+
+    } else if (botStyle === "scissors") {
+        document.getElementById("bot-js-scissors").style.boxShadow = "4px 4px 8px red";
+
+        document.getElementById("bot-js-paper").style.boxShadow = "none";
+        document.getElementById("bot-js-rock").style.boxShadow = "none";
+        document.getElementById("bot-js-lizard").style.boxShadow = "none";
+        document.getElementById("bot-js-spock").style.boxShadow = "none";
+
+    } else if (botStyle === "lizard") {
+        document.getElementById("bot-js-lizard").style.boxShadow = "4px 4px 8px red";
+
+        document.getElementById("bot-js-paper").style.boxShadow = "none";
+        document.getElementById("bot-js-scissors").style.boxShadow = "none";
+        document.getElementById("bot-js-rock").style.boxShadow = "none";
+        document.getElementById("bot-js-spock").style.boxShadow = "none";
+
+    } else if (botStyle === "spock") {
+        document.getElementById("bot-js-spock").style.boxShadow = "4px 4px 8px red";
+
+        document.getElementById("bot-js-paper").style.boxShadow = "none";
+        document.getElementById("bot-js-scissors").style.boxShadow = "none";
+        document.getElementById("bot-js-lizard").style.boxShadow = "none";
+        document.getElementById("bot-js-rock").style.boxShadow = "none";
+
+    } else {
+        console.log("botStyling() something went wrong");
+    }
+
+
+    console.log("botStyling() Function");
 }
 
 
